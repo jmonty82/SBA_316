fb050ca1d230f0467fa6df67e48a4a47dbd5cde3
jmonty82 <jmontrose82@gmail.com>
2026-05-31 04:49:31 -0400

    First Commit

---
+++ 316.js
@@ -0,0 +1,7 @@
+// document.createElement("div1");
+const wordBox = [
+"obtuse", "rubber", "goose", "green", "moose", "guava",
+"juice", "giant", "snake", "birthday", "cake", "large",
+"fries", "chocolate", "shake"
+]
+document.
